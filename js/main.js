7e11522c1d1acc8ddf13fdde0779fa3071bf9318
AppleOVip <96415077+AppleOVip@users.noreply.github.com>
2026-05-27 14:33:22 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const scrollIndicator = document.getElementById('scroll-indicator');
+const floatingButtons = document.getElementById('floating-buttons');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        
+        if(scrollIndicator) {
+            scrollIndicator.classList.add('opacity-0', 'pointer-events-none');
+        }
+        
+        if(floatingButtons) {
+            floatingButtons.classList.remove('opacity-0', 'translate-y-10', 'pointer-events-none');
+            floatingButtons.classList.add('opacity-100', 'translate-y-0');
+        }
+
+    } else {
+        
+        if(scrollIndicator) {
+            scrollIndicator.classList.remove('opacity-0', 'pointer-events-none');
+        }
+        
+        if(floatingButtons) {
+            floatingButtons.classList.add('opacity-0', 'translate-y-10', 'pointer-events-none');
+            floatingButtons.classList.remove('opacity-100', 'translate-y-0');
+        }
+    }
+});
